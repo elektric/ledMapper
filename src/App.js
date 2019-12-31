5c913ee1e891e9ba5a292f653e3934fcb4e3430d
kalsowje <kalsowje@gmail.com>
2019-12-30 20:48:10 -0600

    added a rectangle

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import konva from 'konva'
+import { Stage, Layer, Rect, Image } from 'react-konva'
+import Led from './Led'
 
-function App() {
+function App () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Stage width={window.innerWidth} height={window.innerHeight}>
+        <Layer>
+          <Led />
+        </Layer>
+      </Stage>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
